0f353b521625ef3569383a939b59775fb57d716e
AfnanDhalait <afnandhalait1@gmail.com>
2025-12-06 12:40:30 +0530

    Update aplication.java

--- aplication.java
+++ aplication.java
@@ -1,1 +1,37 @@
+// Sample JavaScript Program
+// Author: Your Name
+
+// Function to greet a user
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to JavaScript.`;
+}
+
+// Example: Using the function
+let userName = "John";
+console.log(greetUser(userName));
+
+// Basic arithmetic function
+function addNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addNumbers(10, 20);
+console.log("The sum is:", sum);
+
+// Working with arrays
+let colors = ["Red", "Green", "Blue"];
+colors.push("Yellow");
+console.log("Color List:", colors);
+
+// Simple object
+let person = {
+    name: "Alice",
+    age: 25,
+    isStudent: true,
+    greet() {
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+};
+
+person.greet();
 
